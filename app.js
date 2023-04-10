b479f6ca9b6a8582ac8e5af1d2ebde0eaa9e79af
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-04-11 03:21:27 +0600

    players part done

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-// Get all select buttons
 const selectBtns = document.querySelectorAll(".select-btn");
 const ul = document.getElementById("player-list");
 const allPlayerNumber = [];
 
-// Add event listener to each select button
 selectBtns.forEach((selectBtn) => {
   selectBtn.addEventListener("click", () => {
-    // Get the player name
     const playerName = selectBtn
       .closest(".text-center")
       .querySelector(".player-name").innerText;
