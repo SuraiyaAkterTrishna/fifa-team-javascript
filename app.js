66a1330bef4f063024a3af17064ccf4b47a85da5
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-04-11 03:19:49 +0600

    button disable after selecting

--- app.js
+++ app.js
@@ -1,19 +1,7 @@
-/* function playerName(){
-    const playerNameElement = document.getElementsByClassName('player-name');
-    for (const player of playerNameElement) {
-        return player;
-    }
-} */
-// const selectButtons = document.getElementsByClassName('select-btn');
-// for (const button of selectButtons) {
-//     button.addEventListener('click', function(){
-//         console.log(playerName());
-//     });
-// };
-
 // Get all select buttons
 const selectBtns = document.querySelectorAll(".select-btn");
 const ul = document.getElementById("player-list");
+const allPlayerNumber = [];
 
 // Add event listener to each select button
 selectBtns.forEach((selectBtn) => {
@@ -23,9 +11,13 @@ selectBtns.forEach((selectBtn) => {
       .closest(".text-center")
       .querySelector(".player-name").innerText;
     if (ul.childElementCount < 5) {
+      selectBtn.setAttribute('disabled', true);
+      selectBtn.classList.add('disabled');
       const li = document.createElement("li");
       li.innerText = playerName;
       ul.appendChild(li);
+      allPlayerNumber.push(li);
+      console.log(allPlayerNumber);
     }
     else {
         alert('You can only select up to five players!');
